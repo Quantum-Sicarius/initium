d55dc3a9c47874b8f4535ffe254fa13b2bbf95dd
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2019-01-30 14:30:27 +0200

    Intial configuration changes.y

--- roles/mirrorlist/files/reflector-update.sh
+++ roles/mirrorlist/files/reflector-update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-/usr/bin/reflector --latest 20 \
+/usr/bin/reflector --latest 100 \
                    --sort rate \
                    --protocol https \
+                   --protocol http \
                    --save /etc/pacman.d/mirrorlist
                    
 if [[ -f /etc/pacman.d/mirrorlist.pacnew ]]; then
